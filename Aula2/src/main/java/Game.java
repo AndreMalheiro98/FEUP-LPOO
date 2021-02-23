13d4f40259afb9338569ca7bb519fe15a02f4d35
André Malheiro <andremalheiro@DESKTOP-93HIJFK.Home>
2021-02-23 16:32:28 +0000

    Second class - game looping correctly

--- Aula2/src/main/java/Game.java
+++ Aula2/src/main/java/Game.java
@@ -4,15 +4,29 @@ import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.input.KeyStroke;
 
 import java.io.IOException;
 
 public class Game {
     /*-----------------------------Class Variables-----------------------------*/
     private Screen screen;
+    private boolean runTime;
+    private int x;
+    private int y;
 
+    /*-----------------------------Class Variables Setters and Getters---------*/
+        public boolean getRunTime(){
+            return this.runTime;
+        }
+        public void setRunTime(boolean bool){
+            this.runTime=bool;
+        }
     /*-----------------------------Class constructor---------------------------*/
     public Game(){
+        this.x=10;
+        this.y=10;
+        this.runTime=true;
         try {
             TerminalSize terminalSize = new TerminalSize(50, 30);
             DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
@@ -32,16 +46,47 @@ public class Game {
     /*-----------------------Class Functions----------------------------------*/
     private void draw() throws IOException{
             this.screen.clear();
-            this.screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+            KeyStroke key= screen.readInput();
+            this.processKey(key);
+            this.screen.setCharacter(this.x, this.y, TextCharacter.fromCharacter('X')[0]);
             this.screen.refresh();
     }
 
-    public void run(){
-        try{
-            this.draw();
-        }catch (IOException err){
-            err.printStackTrace();
+    private void processKey(KeyStroke key) throws IOException {
+        switch (key.getKeyType()){
+            case ArrowLeft:
+                this.x--;
+                break;
+            case ArrowRight:
+                this.x++;
+                break;
+            case ArrowUp:
+                this.y--;
+                break;
+            case ArrowDown:
+                this.y++;
+                break;
+            case Character:
+                if(key.getCharacter()=='q')
+                    this.screen.close();
+                break;
+            case EOF:
+                this.setRunTime(false);
+                break;
+            default:
+                break;
         }
+    }
+
+    public void run(){
+        while(this.getRunTime()){
+            try{
+                this.draw();
+            }catch (IOException err){
+                err.printStackTrace();
+            }
+        }
+
     }
 
 }
