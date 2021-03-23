28063534265f8addb732a9d78fbbc36745743172
Andre Malheiro <andre.pedro.malheiro@gmail.com>
2021-03-23 16:35:45 +0000

    Composite method done

---
+++ Aula5/untitled1/src/main/java/StringTransformerGroup.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class StringTransformerGroup implements StringTransformer{
+    List<StringTransformer> transformersList;
+
+    public StringTransformerGroup(List<StringTransformer> transformers){
+        this.transformersList=transformers;
+    }
+
+    @Override
+    public void execute(StringDrink drink) {
+        for(StringTransformer transformer: this.transformersList)
+            transformer.execute(drink);
+    }
+}
